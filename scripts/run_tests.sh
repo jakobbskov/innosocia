3ffdd0804404f6e3ad8147f52845c8f98084dece
jakobbskov <jakobskov@protonmail.com>
2025-06-06 14:33:31 +0200

    Use array for shellcheck files

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -13,9 +13,9 @@ if ! command -v yamllint >/dev/null; then
 fi
 
 echo "Running ShellCheck..."
-sh_files=$(git ls-files '*.sh')
-if [ -n "$sh_files" ]; then
-  shellcheck "$sh_files"
+mapfile -t sh_files < <(git ls-files '*.sh')
+if [ ${#sh_files[@]} -gt 0 ]; then
+  shellcheck "${sh_files[@]}"
 else
   echo "No shell scripts to check."
 fi
